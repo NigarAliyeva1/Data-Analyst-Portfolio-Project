628edd6623a56f9216ac8b3e18a809ee87d812ed
Nigarkhanim Aliyeva <112957859+NigarAliyeva1@users.noreply.github.com>
2024-02-19 22:58:02 +0400

    Update PercentagePopulationInfected.sql
    Add Continents with Highest Infection Rate compared to Population

--- PercentagePopulationInfected.sql
+++ PercentagePopulationInfected.sql
@@ -12,3 +12,15 @@ From PortfolioProject..CovidDeaths
 where location like '%Azerbaijan%' 
 group by location, population
 order by PercentagePopulationInfected desc
+
+  
+--Continents with Highest Infection Rate compared to Population
+select distinct continent, SUM(population) as SumPopulation , MAX(total_cases) as HighestInfectedCount, MAX(cast(total_cases as float)/cast(population as float))*100 as PercentagePopulationInfected
+From PortfolioProject..CovidDeaths
+where continent is not null
+group by continent
+order by PercentagePopulationInfected desc
+
+
+
+
