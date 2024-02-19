09d0bc71503f098ba5e4e8dc5e1855723f87691d
Nigarkhanim Aliyeva <112957859+NigarAliyeva1@users.noreply.github.com>
2024-02-19 23:43:46 +0400

    Update TotalDeathCount.sql
    Add Contintents with the Highest Death Count per Population

--- TotalDeathCount.sql
+++ TotalDeathCount.sql
@@ -1,8 +1,26 @@
--- Countries with Highest Death Count per Population
+-- Countries with the Highest Death Count per Population
 
 Select TOP 5 Location, Population, MAX(cast(Total_deaths as int)) as TotalDeathCount
 From PortfolioProject..CovidDeaths
 Where continent is not null 
 Group by Location, Population
 Order by TotalDeathCount desc
+
+
+-- Contintents with the Death Count per Population
+
+Select continent, MAX(cast(Total_deaths as int)) as TotalDeathCount
+From PortfolioProject..CovidDeaths
+Where continent is not null 
+Group by continent
+Order by TotalDeathCount desc
+
+
+-- The Contintent with the Highest Death Count per Population
+  
+Select TOP 1 continent, MAX(cast(Total_deaths as int)) as TotalDeathCount
+From PortfolioProject..CovidDeaths
+Where continent is not null 
+Group by continent
+order by TotalDeathCount desc
 
