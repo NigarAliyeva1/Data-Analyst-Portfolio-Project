5b3518bfc707edc921d100de4c1147e8588eb0c6
Nigarkhanim Aliyeva <112957859+NigarAliyeva1@users.noreply.github.com>
2024-02-20 19:52:48 +0400

    Create PeopleVaccinated.sql

---
+++ PeopleVaccinated.sql
@@ -0,0 +1,10 @@
+-- Shows Percentage of Population that has recieved at least one Covid Vaccine
+
+Select dea.continent, dea.location, dea.date, dea.population, vac.new_vaccinations
+, SUM(CONVERT(float,vac.new_vaccinations)) OVER (Partition by dea.location Order by dea.location, dea.Date) as PeopleVaccinated
+From PortfolioProject..CovidDeaths dea
+Join PortfolioProject..CovidVaccinations vac
+	On dea.location = vac.location
+	and dea.date = vac.date
+where dea.continent is not null and vac.new_vaccinations is not null
+order by 2,3
