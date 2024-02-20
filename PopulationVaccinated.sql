9e8f34163bc409f375d220133976a178ec42bfe1
Nigarkhanim Aliyeva <112957859+NigarAliyeva1@users.noreply.github.com>
2024-02-20 21:07:48 +0400

    Update and rename PeopleVaccinated.sql to PopulationVaccinated.sql

--- PopulationVaccinated.sql
+++ PopulationVaccinated.sql
@@ -1,7 +1,7 @@
 -- Shows Percentage of Population that has recieved at least one Covid Vaccine
 
 Select dea.continent, dea.location, dea.date, dea.population, vac.new_vaccinations
-, SUM(CONVERT(float,vac.new_vaccinations)) OVER (Partition by dea.location Order by dea.location, dea.Date) as PeopleVaccinated
+, SUM(CONVERT(float,vac.new_vaccinations)) OVER (Partition by dea.location Order by dea.location, dea.Date) as PopulationVaccinated
 From PortfolioProject..CovidDeaths dea
 Join PortfolioProject..CovidVaccinations vac
 	On dea.location = vac.location
