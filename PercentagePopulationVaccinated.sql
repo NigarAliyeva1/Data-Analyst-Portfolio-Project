63bed8a9af74a9d473131325d7f03305ecc727db
Nigarkhanim Aliyeva <112957859+NigarAliyeva1@users.noreply.github.com>
2024-02-20 21:06:26 +0400

    Create PercentagePopulationVaccinated.sql

---
+++ PercentagePopulationVaccinated.sql
@@ -0,0 +1,45 @@
+-- Using CTE to show Percentage of Population Vaccinated
+
+With PopulationVaccin (Continent, Location, Date, Population, New_Vaccinations, PopulationVaccinated)
+as
+(
+Select dea.continent, dea.location, dea.date, dea.population, vac.new_vaccinations
+, SUM(CONVERT(float,vac.new_vaccinations)) OVER (Partition by dea.Location Order by dea.location, dea.Date) as PopulationVaccinated
+From PortfolioProject..CovidDeaths dea
+Join PortfolioProject..CovidVaccinations vac
+	On dea.location = vac.location
+	and dea.date = vac.date
+where dea.continent is not null 
+)
+Select *, (PopulationVaccinated/Population)*100 as PercentagePopulationVaccinated
+From PopulationVaccin
+order by Location, Date
+
+
+-- Using Temp Table to show Percentage of Population Vaccinated
+
+DROP Table if exists #PercentPopulationVaccinated
+Create Table #PercentPopulationVaccinated
+(
+Continent nvarchar(255),
+Location nvarchar(255),
+Date datetime,
+Population numeric,
+New_vaccinations numeric,
+PopulationVaccinated numeric
+)
+
+Insert into #PercentPopulationVaccinated
+Select dea.continent, dea.location, dea.date, dea.population, vac.new_vaccinations
+, SUM(CONVERT(float,vac.new_vaccinations)) OVER (Partition by dea.Location Order by dea.location, dea.Date) as PopulationVaccinated
+From PortfolioProject..CovidDeaths dea
+Join PortfolioProject..CovidVaccinations vac
+	On dea.location = vac.location
+	and dea.date = vac.date
+where dea.continent is not null 
+
+
+Select *, (PopulationVaccinated/Population)*100 as PercentagePopulationVaccinated
+From #PercentPopulationVaccinated
+order by Location, Date
+
